4e34d2bc6aa07a2aae9bd1a8849a9a3571a99d63
KaleChip24 <oharakale@gmail.com>
2022-07-16 12:39:40 -0400

    moved chack_card_form.html into the cards folder to fix the broken path, added 'iknow' and 'i dont know' buttons

--- cards/views.py
+++ cards/views.py
@@ -4,10 +4,11 @@ from django.views.generic import (
   CreateView,
   UpdateView,
 )
+from django.shortcuts import get_object_or_404, redirect
 
 import random
 
-# from .forms import CardCheckForm
+from .forms import CardCheckForm
 from .models import Card
 
 class CardListView(ListView):
@@ -24,7 +25,7 @@ class CardUpdateView(CardCreateView, UpdateView):
   
 class BoxView(CardListView):
   template_name = "/cards/box.html"
-  # form_class = CardCheckForm
+  form_class = CardCheckForm
   
   def get_queryset(self):
       return Card.objects.filter(box=self.kwargs["box_num"])
@@ -35,3 +36,11 @@ class BoxView(CardListView):
     if self.object_list:
       context["check_card"] = random.choice(self.object_list)
     return context
+  
+  def post(self, request, *arg, **kwargs):
+    form = self.form_class(request.POST)
+    if form.is_valid():
+      card = get_object_or_404(Card, id=form.cleaned_data["card_id"])
+      card.move(form.cleaned_data["solved"])
+    
+    return redirect(request.META.get("HTTP_REFERER"))
